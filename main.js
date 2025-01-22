b0634ebb210fbb602a4303e7b8e05f88a68ef4c3
Nathan <nathan60107@yahoo.com.tw>
2025-01-22 19:41:18 +0800

    fix: update version

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  修改Now的樣式，讓他在網頁版從無法直視變得勉強可看
 // @namespace    nathan60107
 // @author       nathan60107(貝果)
-// @version      1.0.0
+// @version      1.1.0
 // @homepage     https://home.gamer.com.tw/profile/index_creation.php?owner=nathan60107&folder=425332
 // @match        https://forum.gamer.com.tw/B.php*
 // @match        https://now.gamer.com.tw/chat_list.php*
