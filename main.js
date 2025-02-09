1a715d480f04e508ce550a1bd8af7103b2fb2f6f
Nathan <nathan60107@yahoo.com.tw>
2025-02-09 15:44:40 +0800

    feat: prevent pull-to-refresh

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // @description  修改Now的樣式，讓他在網頁版從無法直視變得勉強可看
 // @namespace    nathan60107
 // @author       nathan60107(貝果)
-// @version      1.2.0
+// @version      1.2.1
 // @homepage     https://home.gamer.com.tw/profile/index_creation.php?owner=nathan60107&folder=425332
 // @match        https://forum.gamer.com.tw/B.php*
 // @match        https://now.gamer.com.tw/chat_list.php*
@@ -23,6 +23,10 @@ GM_addStyle(`
   }
   div[class^="user-runes runes_lv"] {
     display: none !important;
+  }
+  // Prevent "Pull-to-refresh" in mobile device.
+  .chatroom {
+    overscroll-behavior: contain;
   }
 `)
 
